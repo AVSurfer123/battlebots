f39a23571a7424dab486ae4a3f387ac27b9b111e
Matthew Tan <matthtan@stanford.edu>
2023-03-22 22:38:45 -0700

    finished encoders rough draft

--- src/encoders.c
+++ src/encoders.c
@@ -1,5 +1,6 @@
 #include "rpi.h"
 #include "encoders.h"
+#include "cycle-count.h"
 
 int8_t enc_table[16] = {
     0, -1, 1, 0,
@@ -7,31 +8,23 @@ int8_t enc_table[16] = {
     -1, 0, 0, 1,
     0, 1, -1, 0};
 
-void encoder_interrupt()
+int32_t totalCount;
+int enc_val = 0;
+unsigned lastCycleCount = 0;
+MotorData get_encoder_velocity(int A, int B)
 {
-
     int new_val = A << 1 | B;
-
-    // shift new to old
+    // shift new value to old
     enc_val = enc_val << 2;
     enc_val = enc_val | new_val;
 
-    enc_count = enc_count + enc_table[enc_val & 0b1111];
-}
-
-int oldA, oldB;
-int32_t totalCount;
-int32_t velocity;
-
-MotorData get_encoder_velocity(int A, int B)
-{
-
-    // oldA, oldB, A, B
-    oldA = A;
-    oldB = B;
+    int oldTotalCount = totalCount;
+    totalCount = totalCount + enc_table[enc_val & 0b1111];
+    int deltaCount = totalCount - oldTotalCount;
 
     MotorData data;
     data.position = totalCount;
-    data.velocity = velocity;
+    data.velocity = deltaCount / (cycle_cnt_read() - lastCycleCount);
+    lastCycleCount = cycle_cnt_read();
     return data;
 }
