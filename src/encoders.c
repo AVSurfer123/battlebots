4977708d20ca588343b8a1c264c4ce257d9b821e
Matthew Tan <matthtan@stanford.edu>
2023-03-22 22:27:18 -0700

    encoder code

--- src/encoders.c
+++ src/encoders.c
@@ -1,14 +1,15 @@
 #include "rpi.h"
+#include "encoders.h"
 
 int8_t enc_table[16] = {
     0, -1, 1, 0,
     1, 0, 0, -1,
     -1, 0, 0, 1,
-    0, 1, -1, 0
-};
+    0, 1, -1, 0};
 
-void encoder_interrupt() {
-    
+void encoder_interrupt()
+{
+
     int new_val = A << 1 | B;
 
     // shift new to old
@@ -18,6 +19,19 @@ void encoder_interrupt() {
     enc_count = enc_count + enc_table[enc_val & 0b1111];
 }
 
-int get_encoder_velocity()
+int oldA, oldB;
+int32_t totalCount;
+int32_t velocity;
+
+MotorData get_encoder_velocity(int A, int B)
 {
+
+    // oldA, oldB, A, B
+    oldA = A;
+    oldB = B;
+
+    MotorData data;
+    data.position = totalCount;
+    data.velocity = velocity;
+    return data;
 }
